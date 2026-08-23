fe06c2166fa9938aa3e7adf285a309b1eca1ffce
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-23 21:07:18 +0530

    Step 18: Log

--- model.py
+++ model.py
@@ -208,8 +208,17 @@ class Relu(Function):
         mask = grad.lazybuffer_binary_e(BinaryOps.CMPLT, self.ret)
         return grad_output.lazybuffer_binary_e(BinaryOps.MUL, mask)
 
-# Step 18 - Log (not yet solved)
-# TODO: implement
+# Step 18 - Log
+class Log(Function):
+    def forward(self, x):
+        # TODO: return the natural log of x and save x for backward
+        self.x = x
+        return x.e(UnaryOps.LOG)
+
+
+    def backward(self, grad_output):
+        # TODO: return the gradient of log with respect to its input
+        return grad_output.lazybuffer_binary_e(BinaryOps.DIV, self.x)
 
 # Step 19 - Exp (not yet solved)
 # TODO: implement
