026b7602055f57da57a486c75b1f2d173f52b7ba
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-23 09:42:06 +0530

    Step 2: argsort

--- model.py
+++ model.py
@@ -15,8 +15,10 @@ def prod(shape):
 
     return product
 
-# Step 2 - argsort (not yet solved)
-# TODO: implement
+# Step 2 - argsort
+def argsort(values):
+    # TODO: Return the indices that would sort values in ascending order.
+    return sorted(range(len(values)), key=lambda x: values[x])
 
 # Step 3 - make_op_enums (not yet solved)
 # TODO: implement
